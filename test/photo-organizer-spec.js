7571bd59d6afb915ae4b3afcbe6ef5a493eac090
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-08-29 21:02:56 +0200

    impl: perform all operations on per file basis

--- test/photo-organizer-spec.js
+++ test/photo-organizer-spec.js
@@ -39,72 +39,21 @@ describe("Photo organizer", function() {
 
   });
 
-  it('retrieves exif data for all photos', function (done) {
+  it('organizes all photo files into month directories', function (done) {
     var organizer = new PhotoOrganizer(mockFs, exifReaderFake);
 
-    organizer.retrieveExifData([
-      'a/b/c.jpg',
-      'a/b/d.jpg',
-      'a/b/e.jpg'
-    ])
-    .then(function () {
-      var dirs = organizer._filesToMonthDirNames;
-      expect(_.keys(dirs).length).toBe(3);
-
-      _.each(_.keys(dirs), function (file) {
-        expect(dirs[file]).toBe(moment(testDates[file]).format('YYYY-MM'));
-      });
-
-    })
-    .done(function () {
-      done();
-    });
-
-  });
-
-  it('create directories in the target directory based on months when photos were taken', function (done) {
-    var organizer = new PhotoOrganizer(mockFs, exifReaderFake);
-    organizer._filesToMonthDirNames = {
-      'a/b/c.jpg': '2014-06',
-      'a/b/d.jpg': '2014-07',
-      'a/b/e.jpg': '2014-08'
-    };
-
-    organizer.createMonthSubdirectories('target')
-    .then(function () {
-      var actualDirs = _.keys(mockFs._root._entries['target']._entries).sort();
-      var expectedDirs = _.map(_.values(testDates), function (date) {
+    var same = function (x) { return x; };
+    var files = _.sortBy(_.keys(testDates), same);
+    var dirNames = _(_.values(testDates))
+      .map(function (date) {
         return moment(date).format('YYYY-MM');
-      });
+      })
+      .sortBy(same)
+      .value();
 
-      expectedDirs.sort();
+    var monthDirNames = _.zipObject(files, dirNames);
 
-      expect(actualDirs.length).toBe(expectedDirs.length);
-      _.each(actualDirs, function (dir, i) {
-        expect(dir).toBe(expectedDirs[i]);
-      });
-
-    })
-    .done(function () {
-      done();
-    });
-
-  });
-
-  it('scatters photos into respective month directories', function (done) {
-    var organizer = new PhotoOrganizer(mockFs, exifReaderFake);
-    var monthDirNames = organizer._filesToMonthDirNames = {
-      'a/b/c.jpg': '2014-06',
-      'a/b/d.jpg': '2014-07',
-      'a/b/e.jpg': '2014-08'
-    };
-
-    var files = _.keys(organizer._filesToMonthDirNames);
-
-    organizer.createMonthSubdirectories('target')
-    .then(function () {
-      return organizer.scatterPhotos('target');
-    })
+    organizer.organize('a/b', 'target')
     .then(function () {
       _.each(files, function (file, i) {
         var fname = mockFs.base(file);
